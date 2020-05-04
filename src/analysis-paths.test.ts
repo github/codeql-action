572c8bbc0c9710ff2dbfbf82affb643f021bd3fd
Robert Brignull <robertbrignull@gmail.com>
2020-05-04 18:28:01 +0100

    switch to using ava

--- src/analysis-paths.test.ts
+++ src/analysis-paths.test.ts
@@ -1,18 +1,20 @@
+import test from 'ava';
+
 import * as analysisPaths from './analysis-paths';
 import * as configUtils from './config-utils';
 
-test("emptyPaths", async () => {
+test("emptyPaths", async t => {
     let config = new configUtils.Config();
     analysisPaths.includeAndExcludeAnalysisPaths(config, []);
-    expect(process.env['LGTM_INDEX_INCLUDE']).toBeUndefined();
-    expect(process.env['LGTM_INDEX_EXCLUDE']).toBeUndefined();
+    t.is(process.env['LGTM_INDEX_INCLUDE'], undefined);
+    t.is(process.env['LGTM_INDEX_EXCLUDE'], undefined);
 });
 
-test("nonEmptyPaths", async () => {
+test("nonEmptyPaths", async t => {
     let config = new configUtils.Config();
     config.paths.push('path1', 'path2');
     config.pathsIgnore.push('path3', 'path4');
     analysisPaths.includeAndExcludeAnalysisPaths(config, []);
-    expect(process.env['LGTM_INDEX_INCLUDE']).toEqual('path1\npath2');
-    expect(process.env['LGTM_INDEX_EXCLUDE']).toEqual('path3\npath4');
+    t.is(process.env['LGTM_INDEX_INCLUDE'], 'path1\npath2');
+    t.is(process.env['LGTM_INDEX_EXCLUDE'], 'path3\npath4');
 });

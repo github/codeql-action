e7881deb831a611fabb5dc4169d8374decc8c705
Robert Brignull <robertbrignull@gmail.com>
2020-06-26 12:25:01 +0100

    fix order of imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core';
 
-import * as analyze from './finalize-db';
 import * as autobuild from './autobuild';
+import * as analyze from './finalize-db';
 import * as init from './setup-tracer';
 import * as upload_sarif from './upload-sarif';
 

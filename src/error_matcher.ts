3e6d23928bb590e0714fcbd23a8fa3ea01aaa5cb
Nick Fyson <nickfyson@github.com>
2020-09-07 22:50:37 +0100

    add test for initial error matcher

--- src/error_matcher.ts
+++ src/error_matcher.ts
@@ -1,19 +1,19 @@
 
 export type ErrorMatcher = [number|null, RegExp|null, string];
 
-const namedMatchers: { [key: string]: ErrorMatcher } = {
+// exported only for testing purposes
+export const namedMatchersForTesting: { [key: string]: ErrorMatcher } = {
+  /*
+  In due course it may be possible to remove the regex, if/when javascript also exits with code 32.
+   For context see https://github.com/github/semmle-code/pull/36921
+  */
   noSourceCodeFound: [
     32,
-    null,
-    `No source code was seen during the build. Please see...
-https://docs.github.com/en/github/finding-security-vulnerabilities-and-errors-in-your-code/troubleshooting-code-scanning#no-code-found-during-the-build`
-  ],
-  noSourceCodeFoundJavascript: [
-    null,
     new RegExp("No JavaScript or TypeScript code found\\."),
     `No source code was seen during the build. Please see...
 https://docs.github.com/en/github/finding-security-vulnerabilities-and-errors-in-your-code/troubleshooting-code-scanning#no-code-found-during-the-build`
-  ]
+  ],
 };
 
-export const errorMatchers = Object.values(namedMatchers);
+// we collapse the matches into an array for use in exec_wrapper
+export const errorMatchers = Object.values(namedMatchersForTesting);

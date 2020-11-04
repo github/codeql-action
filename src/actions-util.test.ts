5c0e2f93f11eacc5890d159b35ee35865962a768
Simon Engledew <simon-engledew@github.com>
2020-11-04 09:37:42 +0000

    Fix invalid API call when running locally
    The method getAnalysisKey would call getWorkflowPath and raise an API Error.
    
    This change follows the pattern in prepareLocalRunEnvironment to set a dummy value for the required environment variable, therefore shortcutting the API request.

--- src/actions-util.test.ts
+++ src/actions-util.test.ts
@@ -33,16 +33,34 @@ test("getRef() returns head PR ref if GITHUB_SHA not currently checked out", asy
   t.deepEqual(actualRef, "refs/pull/1/head");
 });
 
+test("getAnalysisKey() when a local run", async (t) => {
+  const origLocalRun = process.env.CODEQL_LOCAL_RUN;
+
+  process.env.CODEQL_LOCAL_RUN = "true";
+  process.env.CODEQL_ACTION_ANALYSIS_KEY = "";
+  process.env.GITHUB_JOB = "";
+
+  actionsutil.prepareLocalRunEnvironment();
+
+  const actualAnalysisKey = await actionsutil.getAnalysisKey();
+
+  t.deepEqual(actualAnalysisKey, "LOCAL-RUN:UNKNOWN-JOB");
+
+  process.env.CODEQL_LOCAL_RUN = origLocalRun;
+});
+
 test("prepareEnvironment() when a local run", (t) => {
   const origLocalRun = process.env.CODEQL_LOCAL_RUN;
 
   process.env.CODEQL_LOCAL_RUN = "false";
   process.env.GITHUB_JOB = "YYY";
+  process.env.CODEQL_ACTION_ANALYSIS_KEY = "TEST";
 
   actionsutil.prepareLocalRunEnvironment();
 
   // unchanged
   t.deepEqual(process.env.GITHUB_JOB, "YYY");
+  t.deepEqual(process.env.CODEQL_ACTION_ANALYSIS_KEY, "TEST");
 
   process.env.CODEQL_LOCAL_RUN = "true";
 
@@ -50,13 +68,24 @@ test("prepareEnvironment() when a local run", (t) => {
 
   // unchanged
   t.deepEqual(process.env.GITHUB_JOB, "YYY");
+  t.deepEqual(process.env.CODEQL_ACTION_ANALYSIS_KEY, "TEST");
+
+  process.env.CODEQL_ACTION_ANALYSIS_KEY = "";
+
+  actionsutil.prepareLocalRunEnvironment();
+
+  // updated
+  t.deepEqual(process.env.GITHUB_JOB, "YYY");
+  t.deepEqual(process.env.CODEQL_ACTION_ANALYSIS_KEY, "LOCAL-RUN:YYY");
 
   process.env.GITHUB_JOB = "";
+  process.env.CODEQL_ACTION_ANALYSIS_KEY = "";
 
   actionsutil.prepareLocalRunEnvironment();
 
   // updated
   t.deepEqual(process.env.GITHUB_JOB, "UNKNOWN-JOB");
+  t.deepEqual(process.env.CODEQL_ACTION_ANALYSIS_KEY, "LOCAL-RUN:UNKNOWN-JOB");
 
   process.env.CODEQL_LOCAL_RUN = origLocalRun;
 });

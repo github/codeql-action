56b1ead679faebfa0cf769f08c879ed6acb253d2
Simon Engledew <simon-engledew@github.com>
2020-12-01 17:22:54 +0000

    Do not show lint failed in the UX if it happens

--- src/init-action.ts
+++ src/init-action.ts
@@ -98,7 +98,7 @@ async function run() {
 
     const workflowErrors = await actionsUtil.getWorkflowErrors();
 
-    if (workflowErrors.length > 0) {
+    if (workflowErrors.filter(o => o.code !== 'LintFailed').length > 0) {
       core.warning(actionsUtil.formatWorkflowErrors(workflowErrors));
     }
 

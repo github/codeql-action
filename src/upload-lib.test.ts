bd4e3adfd96f0db552c5299d10b291e416850afb
Robert <robertbrignull@github.com>
2021-01-04 13:12:30 +0000

    Add test of finding SARIF files recursively

--- src/upload-lib.test.ts
+++ src/upload-lib.test.ts
@@ -1,9 +1,12 @@
+import * as fs from "fs";
+import * as path from "path";
+
 import test from "ava";
 
 import { getRunnerLogger } from "./logging";
 import { setupTests } from "./testing-utils";
 import * as uploadLib from "./upload-lib";
-import { GitHubVersion } from "./util";
+import { GitHubVersion, withTmpDir } from "./util";
 
 setupTests(test);
 
@@ -92,4 +95,39 @@ test("validate correct payload used per version", async (t) => {
     t.falsy(payload.base_ref);
     t.falsy(payload.base_sha);
   }
+});
+
+test("finding SARIF files", async (t) => {
+  await withTmpDir(async (tmpDir) => {
+    // include a couple of sarif files
+    fs.writeFileSync(path.join(tmpDir, "a.sarif"), "");
+    fs.writeFileSync(path.join(tmpDir, "b.sarif"), "");
+
+    // other random files shouldn't be returned
+    fs.writeFileSync(path.join(tmpDir, "c.foo"), "");
+
+    // we should recursively look in subdirectories
+    fs.mkdirSync(path.join(tmpDir, "dir1"));
+    fs.writeFileSync(path.join(tmpDir, "dir1", "d.sarif"), "");
+    fs.mkdirSync(path.join(tmpDir, "dir1", "dir2"));
+    fs.writeFileSync(path.join(tmpDir, "dir1", "dir2", "e.sarif"), "");
+
+    // we should ignore symlinks
+    fs.mkdirSync(path.join(tmpDir, "dir3"));
+    fs.symlinkSync(tmpDir, path.join(tmpDir, "dir3", "symlink1"), "dir");
+    fs.symlinkSync(
+      path.join(tmpDir, "a.sarif"),
+      path.join(tmpDir, "dir3", "symlink2.sarif"),
+      "file"
+    );
+
+    const sarifFiles = uploadLib.findSarifFilesInDir(tmpDir);
+
+    t.deepEqual(sarifFiles, [
+      path.join(tmpDir, "a.sarif"),
+      path.join(tmpDir, "b.sarif"),
+      path.join(tmpDir, "dir1", "d.sarif"),
+      path.join(tmpDir, "dir1", "dir2", "e.sarif"),
+    ]);
+  });
 });

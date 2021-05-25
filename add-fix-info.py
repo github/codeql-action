4b68707883aa07dadf9f4ec5bd531f46282b9579
Robert <robertbrignull@github.com>
2021-05-25 18:22:57 +0100

    add add-fix-info.py

---
+++ add-fix-info.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python3 add-fix-info.py <input.sarif>')
+        sys.exit(1)
+
+    with open(sys.argv[1]) as input_file:
+        sarif = json.load(input_file)
+
+    for run in sarif['runs']:
+        for result in run['results']:
+            if result['ruleId'] == 'js/regex/duplicate-in-character-class':
+                if 'fixes' in result:
+                    print('Found result for js/regex/duplicate-in-character-class but result already has fixes')
+                    continue
+
+                print('Adding fix for js/regex/duplicate-in-character-class')
+
+                resultPhysicalLocation = result['locations'][0]['physicalLocation']
+                result['fixes'] = [
+                    {
+                        "description": {
+                            "text": "Remove repeated ':' from character class"
+                        },
+                        "artifactChanges": [
+                            {
+                                "artifactLocation": {
+                                    "uri": resultPhysicalLocation['artifactLocation']['uri']
+                                },
+                                "replacements": [
+                                    {
+                                        "deletedRegion": resultPhysicalLocation['region'],
+                                        "insertedContent": {
+                                            "text": ""
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+    
+    with open(sys.argv[1], 'w') as input_file:
+        json.dump(sarif, input_file, indent = 2)
+
+if __name__ == '__main__':
+    main()

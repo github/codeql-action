065c7165e1285f3bdc2b32626bddc80fad85dfa1
Andrew Eisenberg <aeisenberg@github.com>
2021-12-07 15:03:01 -0800

    Add new file

---
+++ tests/multi-language-repo/main2.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorldApp2 {
+    class Geeks2 {
+        static void Main2(string[] args) {
+            if (true) {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}

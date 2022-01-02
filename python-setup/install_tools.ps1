3e50d096f8ef60fc02a00c455500f89d5fe4d7df
Andrew Eisenberg <aeisenberg@github.com>
2022-01-01 18:15:28 -0800

    Force virtualenv version
    Force the virtualenv version to be 20.11 or less.
    The 20.12 version is failing for python 2 right now.

--- python-setup/install_tools.ps1
+++ python-setup/install_tools.ps1
@@ -5,8 +5,8 @@ py -3 -m pip install --user --upgrade pip setuptools wheel
 
 # virtualenv is a bit nicer for setting up virtual environment, since it will provide up-to-date versions of
 # pip/setuptools/wheel which basic `python3 -m venv venv` won't
-py -2 -m pip install --user virtualenv
-py -3 -m pip install --user virtualenv
+py -2 -m pip install --user 'virtualenv<20.11'
+py -3 -m pip install --user 'virtualenv<20.11'
 
 # poetry 1.0.10 has error (https://github.com/python-poetry/poetry/issues/2711)
 py -3 -m pip install --user poetry!=1.0.10

d61e3fdf0243a8a0c309df6eb18122999df7fe56
Henry Mercer <henrymercer@github.com>
2022-07-12 17:31:31 +0100

    Fix shellcheck errors
    Avoid trying to evaluate `github/codeql-action`.

--- .github/workflows/script/update-required-checks.sh
+++ .github/workflows/script/update-required-checks.sh
@@ -3,16 +3,17 @@
 # Typically, this will be main.
 
 if [ -z "$GITHUB_TOKEN" ]; then
-  echo "Failed: No GitHub token found. This script requires admin access to `github/codeql-action`."
+  echo "Failed: No GitHub token found. This script requires admin access to github/codeql-action."
   exit 1
 fi
 
 if [ "$#" -eq 1 ]; then
-  # If we were passed an argument, pass it as a query to fzf
-  GITHUB_SHA="$@"
+  # If we were passed an argument, use that as the SHA
+  GITHUB_SHA="$0"
 elif [ "$#" -gt 1 ]; then
   echo "Usage: $0 [SHA]"
   echo "Update the required checks based on the SHA, or main."
+  exit 1
 elif [ -z "$GITHUB_SHA" ]; then
   # If we don't have a SHA, use main
   GITHUB_SHA="$(git rev-parse main)"
@@ -21,7 +22,7 @@ fi
 echo "Getting checks for $GITHUB_SHA"
 
 # Ignore any checks with "https://", CodeQL, LGTM, and Update checks.
-CHECKS="$(gh api repos/github/codeql-action/commits/${GITHUB_SHA}/check-runs --paginate | jq --slurp --compact-output --raw-output '[.[].check_runs | .[].name | select(contains("https://") or . == "CodeQL" or . == "LGTM.com" or contains("Update") or contains("update") | not)] | unique | sort')"
+CHECKS="$(gh api repos/github/codeql-action/commits/"${GITHUB_SHA}"/check-runs --paginate | jq --slurp --compact-output --raw-output '[.[].check_runs | .[].name | select(contains("https://") or . == ""CodeQL" or . == "LGTM.com" or contains("Update") or contains("update") | not)] | unique | sort')"
 
 echo "$CHECKS" | jq
 

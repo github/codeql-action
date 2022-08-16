9b7fa3dd991674919f91d3a519d322c454d0ea4a
Angela P Wen <angelapwen@github.com>
2022-08-16 16:27:14 -0700

    Add `expect-error` input to force PR check green on expected failure (#1177)

--- src/actions-util.ts
+++ src/actions-util.ts
@@ -8,6 +8,7 @@ import * as safeWhich from "@chrisgavin/safe-which";
 import * as yaml from "js-yaml";
 
 import * as api from "./api-client";
+import { CODEQL_DEFAULT_ACTION_REPOSITORY } from "./codeql";
 import { Config } from "./config-utils";
 import * as sharedEnv from "./shared-environment";
 import {
@@ -899,4 +900,18 @@ export async function printDebugLogs(config: Config) {
     };
     walkLogFiles(logsDirectory);
   }
+}
+
+// Returns whether workflow kicked off by codeql-action repo itself,
+// or a fork of it.
+export function isAnalyzingCodeQLActionRepoOrFork(): boolean {
+  const codeQLActionRepoUrl = `https://api.github.com/repos/${CODEQL_DEFAULT_ACTION_REPOSITORY}`;
+  const repo = getWorkflowEvent()?.repository;
+  if (repo?.url === codeQLActionRepoUrl) {
+    return true;
+  }
+  if (repo?.fork && repo?.parent?.url === codeQLActionRepoUrl) {
+    return true;
+  }
+  return false;
 }

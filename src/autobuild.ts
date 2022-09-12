40e0374c6ff49c0db274f85701672747de3fa15e
Henry Mercer <henrymercer@github.com>
2022-09-12 18:24:46 +0100

    Ensure backwards compat for multi-language builds with Go reconciliation

--- src/autobuild.ts
+++ src/autobuild.ts
@@ -19,26 +19,70 @@ export async function determineAutobuildLanguages(
   const autobuildLanguages = config.languages.filter((l) =>
     isTracedLanguage(l, isGoExtractionReconciliationEnabled, logger)
   );
-  const language = autobuildLanguages[0];
 
-  if (!language) {
+  if (!autobuildLanguages) {
     logger.info(
       "None of the languages in this project require extra build steps"
     );
     return undefined;
   }
 
-  logger.debug(`Detected dominant traced language: ${language}`);
+  /**
+   * Additionally autobuild Go in the autobuild Action to ensure backwards
+   * compatibility for users performing a multi-language build within a single
+   * job.
+   *
+   * For example, consider a user with the following workflow file:
+   *
+   * ```yml
+   * - uses: github/codeql-action/init@v2
+   *   with:
+   *     languages: go, java
+   * - uses: github/codeql-action/autobuild@v2
+   * - uses: github/codeql-action/analyze@v2
+   * ```
+   *
+   * - With Go extraction disabled, we will run the Java autobuilder in the
+   *   autobuild Action, ensuring we extract both Java and Go code.
+   * - With Go extraction enabled, taking the previous behavior we'd run the Go
+   *   autobuilder, since Go is first on the list of languages. We wouldn't run
+   *   the Java autobuilder at all and so we'd only extract Go code.
+   *
+   * We therefore introduce a special case here such that we'll autobuild Go
+   * in addition to the primary non-Go traced language in the autobuild Action.
+   *
+   * This special case behavior should be removed as part of the next major
+   * version of the CodeQL Action.
+   */
+  const autobuildLanguagesNoGo = autobuildLanguages.filter(
+    (l) => l !== Language.go
+  );
 
-  if (autobuildLanguages.length > 1) {
+  const languages: Language[] = [];
+  // First run the autobuilder for the first non-Go traced language, if one
+  // exists.
+  if (autobuildLanguagesNoGo[0] !== undefined) {
+    languages.push(autobuildLanguagesNoGo[0]);
+  }
+  // If Go is requested, run the Go autobuilder last to ensure it doesn't
+  // interfere with the other autobuilder.
+  if (autobuildLanguages.length !== autobuildLanguagesNoGo.length) {
+    languages.push(Language.go);
+  }
+
+  logger.debug(`Will autobuild ${languages.join(" and ")}.`);
+
+  if (autobuildLanguagesNoGo.length > 1) {
     logger.warning(
-      `We will only automatically build ${language} code. If you wish to scan ${autobuildLanguages
+      `We will only automatically build ${languages.join(
+        " and "
+      )} code. If you wish to scan ${autobuildLanguagesNoGo
         .slice(1)
         .join(" and ")}, you must replace this call with custom build steps.`
     );
   }
 
-  return [language];
+  return languages;
 }
 
 export async function runAutobuild(

f830609a2f54255acbec6de5507eaeeb6a3d2c9d
Angela P Wen <angelapwen@github.com>
2022-11-08 15:12:28 -0800

    Rename custom build test

--- tests/multi-language-repo/Package.swift
+++ tests/multi-language-repo/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "helloWorld",
-            path: "swift-src/helloWorld"
+            path: "swift-custom-build-src/helloWorld"
         )
     ]
 )

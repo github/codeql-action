40cb5622bb53b37e15df8d1b6378cc66fed7c81b
Angela P Wen <angelapwen@github.com>
2022-11-08 15:12:03 -0800

    Add XCode sample project

---
+++ tests/swift-repo/Main/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Main
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tests/swift-repo/Main/MainApp.swift
@@ -0,0 +1,16 @@
+//
+//  MainApp.swift
+//  Main
+//
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

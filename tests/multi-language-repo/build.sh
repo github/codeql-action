ff1fbd64aaa7643ceed3ebfeae10234902c04815
Angela P Wen <angelapwen@github.com>
2022-11-09 10:40:54 -0800

    Test custom build with CodeQL command

--- tests/multi-language-repo/build.sh
+++ tests/multi-language-repo/build.sh
@@ -5,3 +5,7 @@ gcc -o main main.c
 dotnet build -p:UseSharedCompilation=false
 
 javac Main.java
+
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+    codeql db create -l swift test.db
+fi

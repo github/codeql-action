caf1c5057b627542f1ddab85460b685193428121
Rasmus Wriedt Larsen <rasmuswl@github.com>
2022-11-29 13:24:10 +0100

    python-setup: Remove outdated comment

--- python-setup/install_tools.ps1
+++ python-setup/install_tools.ps1
@@ -1,9 +1,5 @@
 #! /usr/bin/pwsh
 
-# while waiting for the next release of `virtualenv` after v20.16.5, we install an older
-# version of `setuptools` to ensure that binaries are always put under
-# `<venv-path>/bin`, which wouldn't always happen with the GitHub actions version of
-# Ubuntu 22.04. See https://github.com/github/codeql-action/issues/1249
 py -2 -m pip install --user --upgrade pip setuptools wheel
 py -3 -m pip install --user --upgrade pip setuptools wheel
 

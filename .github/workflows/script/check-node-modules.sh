cf1855ae373d29880b1ec364f26103902aeaac70
Henry Mercer <henrymercer@github.com>
2023-02-24 20:04:38 +0000

    Fix workflow to update dependencies
    Port over the fix from
    https://github.com/github/codeql-action/pull/1544
    and share code so these scripts don't get out of sync again.

--- .github/workflows/script/check-node-modules.sh
+++ .github/workflows/script/check-node-modules.sh
@@ -7,17 +7,9 @@ if [ ! -z "$(git status --porcelain)" ]; then
     >&2 echo "Failed: Repo should be clean before testing!"
     exit 1
 fi
-# When updating this, make sure to update the npm version in
-# `.github/workflows/update-dependencies.yml` too.
-sudo npm install --force -g npm@9.2.0
 
-# clean the npm cache to ensure we don't have any files owned by root
-sudo npm cache clean --force
+"$(dirname "$0")/update-node-modules.sh" check-only
 
-# Reinstall modules and then clean to remove absolute paths
-# Use 'npm ci' instead of 'npm install' as this is intended to be reproducible
-npm ci
-npm run removeNPMAbsolutePaths
 # Check that repo is still clean
 if [ ! -z "$(git status --porcelain)" ]; then
     # If we get a fail here then the PR needs attention

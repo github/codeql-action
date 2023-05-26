4356c16c33075a4092e2c80a3f6def6939bd3a3e
Michael B. Gale <mbg@github.com>
2023-05-26 11:18:50 +0100

    Add `runResolveBuildEnvironment`

---
+++ src/resolve-environment.ts
@@ -0,0 +1,11 @@
+import { getCodeQL } from "./codeql";
+import { Language } from "./languages";
+import { Logger } from "./logging";
+
+export async function runResolveBuildEnvironment(cmd: string, logger: Logger, language: Language) {
+  logger.startGroup(`Attempting to resolve build environment`);
+  const codeQL = await getCodeQL(cmd);
+  const result = await codeQL.resolveBuildEnvironment(language);
+  logger.endGroup();
+  return result;
+}

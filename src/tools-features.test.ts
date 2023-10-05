db9f2c5968e5bb2a9c2d5346f4166ce5c9c06805
Michael B. Gale <mbg@github.com>
2023-10-05 10:33:33 +0100

    Add test for `isSupportedToolsFeature`

---
+++ src/tools-features.test.ts
@@ -0,0 +1,24 @@
+import test from "ava";
+
+import { makeVersionInfo } from "./testing-utils";
+import { ToolsFeature, isSupportedToolsFeature } from "./tools-features";
+
+test("isSupportedToolsFeature", async (t) => {
+  const versionInfo = makeVersionInfo("1.0.0");
+
+  t.false(
+    isSupportedToolsFeature(
+      versionInfo,
+      ToolsFeature.IndirectTracingSupportsStaticBinaries,
+    ),
+  );
+
+  versionInfo.features = { indirectTracingSupportsStaticBinaries: true };
+
+  t.true(
+    isSupportedToolsFeature(
+      versionInfo,
+      ToolsFeature.IndirectTracingSupportsStaticBinaries,
+    ),
+  );
+});

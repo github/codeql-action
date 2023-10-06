e3a9b9a0ef5ea4274c759b3adecd734021b998eb
Andrew Eisenberg <aeisenberg@github.com>
2023-10-06 12:22:13 -0700

    Updating

--- python-setup/install_tools.ps1
+++ python-setup/install_tools.ps1
@@ -1,5 +1,15 @@
 #! /usr/bin/pwsh
 
+# If we are running greater than or equal to python 3.12, add src to the python path
+Write-Host "Checking python version"
+Write-Host "PYTHONPATH $Env:PYTHONPATH"
+if ((py -3 -c "import sys; print(0 if sys.version_info >= (3, 12) else 1)") -eq "0") {
+    Write-Host "Python 3.12+ detected, adding imp.py to PYTHONPATH"
+    Write-Output "PYTHONPATH=$Env:PYTHONPATH;$pwd/src" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+} else {
+    Write-Host "Python 3.12+ not detected, not adding imp.py to PYTHONPATH"
+}
+
 py -2 -m pip install --user --upgrade pip setuptools wheel
 py -3 -m pip install --user --upgrade pip setuptools wheel
 
@@ -10,13 +20,3 @@ py -3 -m pip install --user virtualenv
 
 py -3 -m pip install --user "poetry>=1.1"
 py -3 -m pip install --user pipenv
-
-
-# If we are running greater than or equal to python 3.12, add src to the python path
-Write-Host "Checking python version"
-if ((py -3 -c "import sys; print(0 if sys.version_info >= (3, 12) else 1)") -eq "0") {
-    Write-Host "Python 3.12+ detected, adding imp.py to PYTHONPATH"
-    Write-Output "PYTHONPATH=$Env:PYTHONPATH;$pwd/src" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
-} else {
-    Write-Host "Python 3.12+ not detected, not adding imp.py to PYTHONPATH"
-}

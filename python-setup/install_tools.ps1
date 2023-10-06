16ccdcacfce9e97d20938fd0f09a7b7abfc48f6d
Andrew Eisenberg <aeisenberg@github.com>
2023-10-06 12:39:40 -0700

    Updating

--- python-setup/install_tools.ps1
+++ python-setup/install_tools.ps1
@@ -2,12 +2,18 @@
 
 # If we are running greater than or equal to python 3.12, add src to the python path
 Write-Host "Checking python version"
-Write-Host "PYTHONPATH $Env:PYTHONPATH"
 if ((py -3 -c "import sys; print(0 if sys.version_info >= (3, 12) else 1)") -eq "0") {
-    Write-Host "Python 3.12+ detected, setting PY_PYTHON3=3.11"
+  Write-Host "Python 3.12+ detected, setting PY_PYTHON3=3.11"
+  py -3.11 -c "import imp"
+  if ($LASTEXITCODE -eq 0) {
+    Write-Host "Python 3.11 detected, using this version instead of 3.12+."
     Write-Output "PY_PYTHON3=3.11" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
+  } else {
+    Write-Host "FAILURE: Python 3.12+ is not supported. Please install Python 3.11."
+    exit 1
+  }
 } else {
-    Write-Host "Python 3.12+ not detected, not PY_PYTHON3"
+  Write-Host "Python 3.12+ not detected, not making any changes."
 }
 
 py -2 -m pip install --user --upgrade pip setuptools wheel

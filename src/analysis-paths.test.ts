45dde380304ce2ff13acdc9ef16c93b40b4769b3
Rasmus Wriedt Larsen <rasmuswl@github.com>
2024-01-04 14:08:57 +0100

    Improve `paths` config handling for JS/Python when globs are used

--- src/analysis-paths.test.ts
+++ src/analysis-paths.test.ts
@@ -64,11 +64,11 @@ test("nonEmptyPaths", async (t) => {
       trapCacheDownloadTime: 0,
     };
     analysisPaths.includeAndExcludeAnalysisPaths(config);
-    t.is(process.env["LGTM_INDEX_INCLUDE"], "path1\npath2");
+    t.is(process.env["LGTM_INDEX_INCLUDE"], undefined);
     t.is(process.env["LGTM_INDEX_EXCLUDE"], "path4\npath5");
     t.is(
       process.env["LGTM_INDEX_FILTERS"],
-      "include:path1\ninclude:path2\ninclude:**/path3\nexclude:path4\nexclude:path5\nexclude:path6/**",
+      "exclude:**/*\ninclude:path1\ninclude:path2\ninclude:**/path3\nexclude:path4\nexclude:path5\nexclude:path6/**",
     );
   });
 });
@@ -98,11 +98,11 @@ test("paths only has globs", async (t) => {
       trapCacheDownloadTime: 0,
     };
     analysisPaths.includeAndExcludeAnalysisPaths(config);
-    t.is(process.env["LGTM_INDEX_INCLUDE"], "");
+    t.is(process.env["LGTM_INDEX_INCLUDE"], undefined);
     t.is(process.env["LGTM_INDEX_EXCLUDE"], undefined);
     t.is(
       process.env["LGTM_INDEX_FILTERS"],
-      "include:path1/**\ninclude:**/path2",
+      "exclude:**/*\ninclude:path1/**\ninclude:**/path2",
     );
   });
 });

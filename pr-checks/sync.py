d67aee1f3efeae2ac8c84f5b39167fb008ed5777
Angela P Wen <angelapwen@github.com>
2024-04-25 11:58:57 -0700

    Add `fail-fast: false` back again

--- pr-checks/sync.py
+++ pr-checks/sync.py
@@ -120,7 +120,8 @@ for file in (this_dir / 'checks').glob('*.yml'):
         'strategy': {
             'matrix': {
                 'include': matrix
-            }
+            },
+            'fail-fast': False,
         },
         'name': checkSpecification['name'],
         'permissions': {

c1eb43eda1801221acb48f7b59e3693ef65ca1cc
Angela P Wen <angelapwen@github.com>
2024-04-25 14:51:03 -0700

    Set up Python 3.11 for all MacOS images

--- pr-checks/sync.py
+++ pr-checks/sync.py
@@ -89,8 +89,7 @@ for file in (this_dir / 'checks').glob('*.yml'):
             # Ensure that this is serialized as a folded (`>`) string to preserve the readability
             # of the generated workflow.
             'if': FoldedScalarString(textwrap.dedent('''
-                    (matrix.os == 'macos-latest' || 
-                    matrix.os == 'macos-12') && (
+                    runner.os == 'macOS' && (
                     matrix.version == 'stable-20230403' ||
                     matrix.version == 'stable-v2.13.5' ||
                     matrix.version == 'stable-v2.14.6')

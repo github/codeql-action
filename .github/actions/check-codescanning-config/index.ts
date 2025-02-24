3b348d9a548445f0f8528a5d94614dd6df264b57
Angela P Wen <angelapwen@github.com>
2025-02-24 13:17:35 -0800

    Debug only: print environment variable

--- .github/actions/check-codescanning-config/index.ts
+++ .github/actions/check-codescanning-config/index.ts
@@ -7,6 +7,7 @@ import * as assert from 'assert'
 const actualConfig = loadActualConfig()
 
 const rawExpectedConfig = process.argv[3].trim()
+core.info("rawExpectedConfig: " + rawExpectedConfig)
 if (!rawExpectedConfig) {
   core.setFailed('No expected configuration provided')
 } else {

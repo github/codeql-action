9a9af90a737e9a7cdaddcb91189c74aba67bedc3
Chuan-kai Lin <cklin@github.com>
2025-07-31 09:27:48 -0700

    Add resolveQueriesStartingPacks()

--- src/codeql.ts
+++ src/codeql.ts
@@ -191,6 +191,10 @@ export interface CodeQL {
   /** Get the location of an extractor for the specified language. */
   resolveExtractor(language: Language): Promise<string>;
   /**
+   * Run 'codeql resolve queries --format=startingpacks'.
+   */
+  resolveQueriesStartingPacks(queries: string[]): Promise<string[]>;
+  /**
    * Run 'codeql github merge-results'.
    */
   mergeResults(
@@ -448,6 +452,10 @@ export function setCodeQL(partialCodeql: Partial<CodeQL>): CodeQL {
     ),
     diagnosticsExport: resolveFunction(partialCodeql, "diagnosticsExport"),
     resolveExtractor: resolveFunction(partialCodeql, "resolveExtractor"),
+    resolveQueriesStartingPacks: resolveFunction(
+      partialCodeql,
+      "resolveQueriesStartingPacks",
+    ),
     mergeResults: resolveFunction(partialCodeql, "mergeResults"),
   };
   return cachedCodeQL;
@@ -943,6 +951,24 @@ export async function getCodeQLForCmd(
         },
       ).exec();
       return JSON.parse(extractorPath) as string;
+    },
+    async resolveQueriesStartingPacks(queries: string[]): Promise<string[]> {
+      const codeqlArgs = [
+        "resolve",
+        "queries",
+        "--format=startingpacks",
+        ...getExtraOptionsFromEnv(["resolve", "queries"]),
+        ...queries,
+      ];
+      const output = await runCli(cmd, codeqlArgs, { noStreamStdout: true });
+
+      try {
+        return JSON.parse(output) as string[];
+      } catch (e) {
+        throw new Error(
+          `Unexpected output from codeql resolve queries --format=startingpacks: ${e}`,
+        );
+      }
     },
     async mergeResults(
       sarifFiles: string[],

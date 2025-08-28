e550de266b82bbba569dc811e27de7213c89ee84
Michael B. Gale <mbg@github.com>
2025-08-28 16:36:26 +0100

    Add utility function to check if code scanning is enabled

--- src/config-utils.ts
+++ src/config-utils.ts
@@ -1490,6 +1490,13 @@ export function generateCodeScanningConfig(
 }
 
 /**
+ * Returns `true` if Code Scanning analysis is enabled, or `false` if not.
+ */
+export function isCodeScanningEnabled(config: Config): boolean {
+  return config.analysisKinds.includes(AnalysisKind.CodeScanning);
+}
+
+/**
  * Returns `true` if Code Quality analysis is enabled, or `false` if not.
  */
 export function isCodeQualityEnabled(config: Config): config is Config & {

055bf49a9977b078d3f26ea273656391e87efe63
Michael B. Gale <mbg@github.com>
2025-08-28 16:30:52 +0100

    Mark `quality-queries` input as deprecated

--- src/init-action.ts
+++ src/init-action.ts
@@ -384,11 +384,21 @@ async function run() {
       }
     }
 
+    // Warn that `quality-queries` is deprecated if there is an argument for it.
+    const qualityQueriesInput = getOptionalInput("quality-queries");
+
+    if (qualityQueriesInput !== undefined) {
+      logger.warning(
+        "The `quality-queries` input is deprecated and will be removed in a future version of the CodeQL Action. " +
+          "Use the `analysis-kinds` input to configure different analysis kinds instead.",
+      );
+    }
+
     config = await initConfig({
       analysisKindsInput: getRequiredInput("analysis-kinds"),
       languagesInput: getOptionalInput("languages"),
       queriesInput: getOptionalInput("queries"),
-      qualityQueriesInput: getOptionalInput("quality-queries"),
+      qualityQueriesInput,
       packsInput: getOptionalInput("packs"),
       buildModeInput: getOptionalInput("build-mode"),
       configFile,

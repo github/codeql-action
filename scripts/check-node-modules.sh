b27a8ef21f72b5c541232d50400874a3f0a374b9
Michael B. Gale <mbg@github.com>
2025-09-25 20:22:47 +0100

    Exit if running in an Actions workflow

--- scripts/check-node-modules.sh
+++ scripts/check-node-modules.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 set -e
+
+# Check if running in GitHub Actions
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  exit 0
+fi
 
 # Check if npm install is likely needed before proceeding
 if [ ! -d node_modules ] || [ package-lock.json -nt node_modules/.package-lock.json ]; then

12e8528a599c2858288525891cfd3d1f72c09689
Nick Rolfe <nickrolfe@github.com>
2025-10-09 12:55:05 +0100

    test

--- src/codeql.ts
+++ src/codeql.ts
@@ -992,14 +992,14 @@ export async function getCodeQLForCmd(
       return JSON.parse(extractorPath) as string;
     },
     async resolveQueriesStartingPacks(queries: string[]): Promise<string[]> {
-      const xcodeqlArgs = [
+      const zcodeqlArgs = [
         "resolve",
         "queries",
         "--format=startingpacks",
         ...getExtraOptionsFromEnv(["resolve", "queries"]),
         ...queries,
       ];
-      const output = await runCli(cmd, xcodeqlArgs, { noStreamStdout: true });
+      const output = await runCli(cmd, zcodeqlArgs, { noStreamStdout: true });
 
       try {
         return JSON.parse(output) as string[];

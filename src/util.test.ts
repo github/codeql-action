fe16891f40875c7dfedef7c09370a00fd299cea8
Henry Mercer <henrymercer@github.com>
2025-10-28 12:40:23 +0000

    Add unit test for `checkDiskUsage`

--- src/util.test.ts
+++ src/util.test.ts
@@ -534,3 +534,12 @@ test("getCgroupCpuCountFromCpus returns undefined if the CPU file exists but is 
     );
   });
 });
+
+test("checkDiskUsage succeeds and produces positive numbers", async (t) => {
+  const diskUsage = await util.checkDiskUsage(getRunnerLogger(true));
+  t.true(
+    diskUsage?.numAvailableBytes !== undefined &&
+      diskUsage.numAvailableBytes > 0,
+  );
+  t.true(diskUsage?.numTotalBytes !== undefined && diskUsage.numTotalBytes > 0);
+});

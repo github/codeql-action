8bfaf96434cd904922ce98ede3c2102622ecb34f
Michael B. Gale <mbg@github.com>
2026-03-01 15:20:30 +0000

    Run `npm ci` in actions

--- pr-checks/sync.sh
+++ pr-checks/sync.sh
@@ -3,5 +3,13 @@ set -e
 
 cd "$(dirname "$0")"
 
-npm install --no-audit --no-fund
+# Run `npm ci` in CI or `npm install` otherwise.
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo "In Actions, running 'npm ci' for 'sync.ts'..."
+  npm ci
+else
+  echo "Running 'npm install' for 'sync.ts'..."
+  npm install --no-audit --no-fund
+fi
+
 npx tsx sync.ts

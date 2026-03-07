0b27c7173152717dc81533bdf1f094a9e68440dc
Michael B. Gale <mbg@github.com>
2026-03-07 00:29:11 +0000

    Generate and use `action-versions.ts`

---
+++ pr-checks/action-versions.ts
@@ -0,0 +1,30 @@
+export const ACTION_VERSIONS = {
+  "actions/checkout": {
+    "version": "v6"
+  },
+  "actions/setup-go": {
+    "version": "v6"
+  },
+  "actions/setup-dotnet": {
+    "version": "v5"
+  },
+  "actions/upload-artifact": {
+    "version": "v7"
+  },
+  "actions/github-script": {
+    "version": "v8"
+  },
+  "actions/setup-python": {
+    "version": "v6"
+  },
+  "actions/setup-java": {
+    "version": "v5"
+  },
+  "actions/setup-node": {
+    "version": "v6"
+  },
+  "ruby/setup-ruby": {
+    "version": "09a7688d3b55cf0e976497ff046b70949eeaccfd",
+    "comment": " v1.288.0"
+  }
+};

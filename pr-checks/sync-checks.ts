9481177f3ded6eaac91e6bb74e30ed0fa45db604
Michael B. Gale <mbg@github.com>
2026-03-13 18:49:50 +0000

    Initialise API client

--- pr-checks/sync-checks.ts
+++ pr-checks/sync-checks.ts
@@ -4,7 +4,21 @@
 
 import { parseArgs } from "node:util";
 
+import * as githubUtils from "@actions/github/lib/utils";
+import { type Octokit } from "@octokit/core";
+import { type PaginateInterface } from "@octokit/plugin-paginate-rest";
+import { type Api } from "@octokit/plugin-rest-endpoint-methods";
+
 import { OLDEST_SUPPORTED_MAJOR_VERSION } from "./config";
+
+/** The type of the Octokit client. */
+type ApiClient = Octokit & Api & { paginate: PaginateInterface };
+
+/** Constructs an `ApiClient` using `token` for authentication. */
+function getApiClient(token: string): ApiClient {
+  const opts = githubUtils.getOctokitOptions(token);
+  return new githubUtils.GitHub(opts);
+}
 
 async function main(): Promise<void> {
   const { values: options } = parseArgs({
@@ -36,6 +50,9 @@ async function main(): Promise<void> {
   console.info(
     `Oldest supported major version is: ${OLDEST_SUPPORTED_MAJOR_VERSION}`,
   );
+
+  // Initialise the API client.
+  const client = getApiClient(options.token);
 
   process.exit(0);
 }

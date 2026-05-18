c05837d3e8b7cc67635eaa027d64ae173ec2b7d5
Michael B. Gale <mbg@github.com>
2026-05-18 19:38:16 +0100

    Scaffold `update-ghes-versions.ts` script

---
+++ pr-checks/update-ghes-versions.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env npx tsx
+
+/**
+ * Updates src/api-compatibility.json with the current range of supported
+ * GitHub Enterprise Server versions by reading the releases.json file from
+ * an `enterprise-releases` checkout.
+ */
+
+function main() {}
+
+// Only call `main` if this script was run directly.
+if (require.main === module) {
+  main();
+}

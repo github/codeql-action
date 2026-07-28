fdf46cd70c0f59d79af585d5c2bf06844e0470ee
Michael B. Gale <mbg@github.com>
2026-07-28 12:32:58 +0100

    Add `ScopedConfigurations` feature

--- src/feature-flags.ts
+++ src/feature-flags.ts
@@ -137,6 +137,8 @@ export enum Feature {
   QaTelemetryEnabled = "qa_telemetry_enabled",
   /** Routes (some) API requests through the registry proxy. */
   ProxyApiRequests = "proxy_api_requests",
+  /** Allows sections specific to analysis kinds in configuration files. */
+  ScopedConfigurations = "scoped_configurations",
   /** Note that this currently only disables baseline file coverage information. */
   SkipFileCoverageOnPrs = "skip_file_coverage_on_prs",
   StartProxyUseFeaturesRelease = "start_proxy_use_features_release",
@@ -383,6 +385,11 @@ export const featureConfig = {
   [Feature.ProxyApiRequests]: {
     defaultValue: false,
     envVar: "CODEQL_ACTION_PROXY_API_REQUESTS",
+    minimumVersion: undefined,
+  },
+  [Feature.ScopedConfigurations]: {
+    defaultValue: false,
+    envVar: "CODEQL_ACTION_SCOPED_CONFIGURATIONS",
     minimumVersion: undefined,
   },
   [Feature.SkipFileCoverageOnPrs]: {
